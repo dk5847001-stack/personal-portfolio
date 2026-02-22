4aa8f8c518984bbb3b0fde7f042bd3aec60a9d6f
Dilkhush Kumar <dk5847001@gmail.com>
2026-02-22 19:39:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function instagram(){
+    window.open("https://www.instagram.com/dilkhush_10star?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==","_blank");
+}
+function facebook(){
+    window.open("https://www.facebook.com/dilkhush.kumar.7549?mibextid=ZbWKwL","_blank");
+}
+
+const menubar = document.querySelector('#menu');
+const Navbar = document.querySelector('.navbar');
+menubar.onclick=()=>{
+    menubar.classList.toggle('bx-x');
+    Navbar.classList.toggle('active')
+}
+const section=document.querySelectorAll('section');
+const navlink = document.querySelectorAll('header nav a')
+window.onscroll = ()=>{
+    section.forEach(sec=>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+        if(top>offset && top < offset + height){
+            sec.classList.add('start-animation');
+            navlink.forEach(links=>{
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*='+id+']').classList.add('active')
+              
+            })
+        }
+    })
+    var header = document.querySelector('.header');
+    header.classList.toggle('sticky',window.scrollY>100)
+    menubar.classList.remove('bx-x');
+    Navbar.classList.remove('active')
+} 
